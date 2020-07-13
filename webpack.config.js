8ca8919b3a9707730b8aba9d40df4f2d524e3d26
Dmitry Mun <mundmitry@gmail.com>
2020-07-13 16:08:17 +0300

    CSS and HTML compiling works

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,51 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: { 
+        main: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: { loader: "babel-loader" },
-            exclude: /node_modules/
-                }
-            ]
-        }
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(ttf|svg|png|jpe?g|gif|woff2?)$/,
+                use: [
+                    'file-loader?name=[path][name].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            disable: true
+                        }
+                    }
+                ]                
+            }
+        ]
+    },
+    plugins: [ 
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+          })
+    ]
 }
