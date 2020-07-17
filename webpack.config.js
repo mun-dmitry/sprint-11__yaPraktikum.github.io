a002ee8f5892ad8abb8d0c76944503d4de938694
Dmitry Mun <mundmitry@gmail.com>
2020-07-18 01:26:10 +0300

    Updated postcss-loader options and readme

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,12 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-                    'css-loader',
+                    {
+                        loader:'css-loader',
+                        options: {
+                            importLoaders: 2
+                        } 
+                    },
                     'postcss-loader'
                 ]
             },
